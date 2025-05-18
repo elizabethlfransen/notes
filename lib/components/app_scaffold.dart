cd28d1cad6aac817980064773e5c14ab874cb68f
Liz Fransen <elizabethlfransen@gmail.com>
2025-05-18 11:33:09 -0700

    Create app scaffold

---
+++ lib/components/app_scaffold.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  const AppScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [
+          SafeArea(
+            child: RawMaterialButton(
+              onPressed: () {},
+              shape: CircleBorder(),
+              child: CircleAvatar(
+                backgroundColor: Colors.grey.shade300,
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Placeholder(),
+    );
+  }
+}
