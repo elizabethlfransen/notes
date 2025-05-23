8bfda47545b8bdb49f79c883d3a01b38621f233d
Liz Fransen <elizabethlfransen@gmail.com>
2025-05-23 13:02:30 -0700

    Add is wasm check

--- lib/home.dart
+++ lib/home.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:notes_app/services/app_title.dart';
 
+const isRunningWithWasm = bool.fromEnvironment('dart.tool.dart2wasm');
+
 class HomeScreen extends HookConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     useAppTitle("Home", ref);
-    return Center(child: Text("Home"));
+
+    return Center(child: Text("Is Running With Wasm: $isRunningWithWasm"));
   }
 }
